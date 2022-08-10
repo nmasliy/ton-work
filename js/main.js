6f48e08d2bef4c52baa2b6c95b6ec829619a8b13
Nick <deckards2505@gmail.com>
2022-08-10 20:58:19 +0600

    add change active nav on scroll

--- js/main.js
+++ js/main.js
@@ -124,55 +124,25 @@ window.addEventListener("DOMContentLoaded", function () {
 				});
 			});
 
-			// Получаем нужный элемент
-
-			var Visible = function (target) {
-				// Все позиции элемента
-				var targetPosition = {
-						top: window.pageYOffset + target.getBoundingClientRect().top,
-						left: window.pageXOffset + target.getBoundingClientRect().left,
-						right: window.pageXOffset + target.getBoundingClientRect().right,
-						bottom: window.pageYOffset + target.getBoundingClientRect().bottom,
-					},
-					// Получаем позиции окна
-					windowPosition = {
-						top: window.pageYOffset,
-						left: window.pageXOffset,
-						right: window.pageXOffset + document.documentElement.clientWidth,
-						bottom: window.pageYOffset + document.documentElement.clientHeight,
-					};
-
-				if (
-					targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
-					targetPosition.top < windowPosition.bottom -100 && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
-					targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
-					targetPosition.left < windowPosition.right
-				) {
-					// Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
-					// Если элемент полностью видно, то запускаем следующий код
+			function checkActiveSection(section) {
+				if(document.documentElement.scrollTop > (section.offsetTop - window.innerHeight / 2 + 100)) {
 					const $active = document.querySelector(".profile__nav li.active");
+					$currentLink = document.querySelector('a[href="#' + section.id + '"]')
+
 					$active.classList.remove("active");
-					$currentLink = document.querySelector('a[href="#' + target.id + '"]')
-					
 					$currentLink.closest("li").classList.add("active");
-
-				} else {
-					// Если элемент не видно, то запускаем этот код
 				}
-			};
+			}
+			window.addEventListener("scroll", function () {
+				const $profile = document.querySelector('#profile')
+				checkActiveSection($profile)
+			});
+			$sections.forEach((item) => {
+				window.addEventListener("scroll", function () {
+					checkActiveSection(item)
+				});
+			});
 
-		// 	$sections.forEach((item) => {
-		// 		// Запускаем функцию при прокрутке страницы
-		// 		window.addEventListener("scroll", function () {
-		// 			Visible(item);
-		// 		});
-
-		// 		// А также запустим функцию сразу. А то вдруг, элемент изначально видно
-		// 		Visible(item);
-		// 	});
-		// 	window.addEventListener("scroll", function () {
-		// 		Visible(document.querySelector('#profile'));
-		// 	});
 		}
 	}
 
