cf35576a918ae9bd36c3d896dbc1c1f801ccaa92
n1colashka <deckards2505@gmail.com>
2022-08-24 12:42:39 +0600

    change categories-list to tags class name

--- js/main.js
+++ js/main.js
@@ -226,6 +226,35 @@ window.addEventListener("DOMContentLoaded", function () {
 			})
 		}
 	}
+
+	function initTabs() {
+		const $tabs = document.querySelectorAll('[data-tab-title]')
+
+		if ($tabs.length > 0) {
+			$tabs.forEach(tab => {
+				tab.addEventListener('click', function (e) {
+					e.preventDefault()
+
+					const activeTab = document.querySelector('[data-tab-title].active')
+					const activeContent = document.querySelector(
+						'[data-tab-content].active'
+					)
+					const id = tab.getAttribute('data-tab')
+
+					if (activeTab) {
+						activeTab.classList.remove('active')
+						activeContent.classList.remove('active')
+					}
+					const content = document.querySelector(
+						'[data-tab-content][data-tab="' + id + '"]'
+					)
+
+					tab.classList.add('active')
+					content.classList.add('active')
+				})
+			})
+		}
+	}
 	
 	initMenu();
 	initCopyProfileLink();
@@ -234,4 +263,5 @@ window.addEventListener("DOMContentLoaded", function () {
 	initModals();
 	initFilter();
 	initSelects();
+	initTabs()
 });
